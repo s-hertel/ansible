d3d0d00322d44aefb0a768333a9754802f3b298c
s-hertel <hertels@berea.edu>
2016-06-20 10:57:51 -0400

    Adding three tests for GalaxyCLI

--- test/units/cli/test_galaxy.py
+++ test/units/cli/test_galaxy.py
@@ -22,6 +22,20 @@ __metaclass__ = type
 from ansible.compat.six import PY3
 from ansible.compat.tests import unittest
 
+from ansible.galaxy.token import GalaxyToken  # added this
+from ansible.cli import CLI  # added this
+from mock import patch
+from mock import MagicMock
+import ansible
+from ansible.errors import AnsibleError, AnsibleOptionsError
+
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
 from nose.plugins.skip import SkipTest
 
 if PY3:
@@ -51,3 +65,58 @@ class TestGalaxy(unittest.TestCase):
         display_result = gc._display_role_info(role_info)
         if display_result.find('\t\tgalaxy_tags:') > -1:
             self.fail('Expected galaxy_tags to be indented twice')
+
+    def test_display_role_info(self):
+        gc = GalaxyCLI(args=self.default_args)
+        galaxy_info = {}
+        role_info = {'name': 'foo', 'description': 'bar'}
+        display_result = gc._display_role_info(role_info)
+        self.assertTrue(display_result == u'\nRole: foo\n\tdescription: bar')
+
+    @patch.object(GalaxyToken, "__init__", return_value=None)  # mocks file being opened/created
+    def test_parse(self, mocked_token):
+        ''' tests that a options parser is created for bin/ansible '''
+        for arguments in [[], ["list", "info"], ["bad_arg", "list"], ["bad_arg", "invalid"]]:    
+            gc = GalaxyCLI(args=arguments)
+            
+            first_arg = False
+            for arg in arguments:
+                if arg in gc.VALID_ACTIONS:
+                    first_arg = arg  # used for testing after the parser is created
+                    break  # stop searching after a valid action is found
+                     
+            if first_arg == False:   # a lack of valid args raises an error
+                with patch('sys.argv', ["-c"]):
+                    self.assertRaises(AnsibleOptionsError, gc.parse)
+            else:
+                with patch('sys.argv', ["-c"]): 
+                    created_parser = gc.parse()
+                self.assertTrue(created_parser == True)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))
+                self.assertTrue(gc.action)
+                self.assertTrue(gc.options.roles_path == ['/etc/ansible/roles'])
+                self.assertTrue(gc.action == first_arg)
+                self.assertNotIn(first_arg, arguments)
+
+    @patch.object(GalaxyToken, "__init__", return_value=None)  # mocks file being opened/created
+    def test_run(self, mocked_token):
+        ''' verifies that the GalaxyCLI object's api is created and that gc.execute is called. '''
+        gc = GalaxyCLI(args=["install"])
+        with patch('sys.argv', ["-c", "-v", '--ignore-errors', 'imaginary_role']):
+            galaxy_parser = gc.parse()
+        gc.execute = MagicMock()
+        with patch.object(CLI, "run", return_value=None) as mock_obj: # mocked to elimate config file message to identify whether file or default is used
+            gc.run()
+        self.assertTrue(gc.execute.called)
+        self.assertTrue(isinstance(gc.api, ansible.galaxy.api.GalaxyAPI))
+
+    @patch.object(GalaxyToken, "__init__", return_value=None)  # mocks file being opened/created
+    def test_exit_without_ignore(self, mock):
+        ''' tests that GalaxyCLI exits with the error specified '''
+        gc = GalaxyCLI(args=["install"])
+        with patch('sys.argv', ["-c", "-v"]):
+            galaxy_parser = gc.parse()
+        with patch.object(CLI, "run", return_value=None) as mock_obj:  # mocked to eliminate config file message to identify whether file or default is used
+            self.assertRaises(AnsibleError, gc.run)
+
