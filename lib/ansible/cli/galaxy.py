713c8bd1655f9744c5ad5187c2a74a581be62850
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-12-10 12:33:10 +0100

    fixup! py2 compat

--- lib/ansible/cli/galaxy.py
+++ lib/ansible/cli/galaxy.py
@@ -520,7 +520,7 @@ class GalaxyCLI(CLI):
                 self.api_servers.append(GalaxyAPI(
                     self.galaxy, 'cmd_arg', cmd_server, token=cmd_token,
                     priority=len(config_servers) + 1,
-                    **galaxy_options,
+                    **galaxy_options
                 ))
         else:
             self.api_servers = config_servers
@@ -530,7 +530,7 @@ class GalaxyCLI(CLI):
             self.api_servers.append(GalaxyAPI(
                 self.galaxy, 'default', C.GALAXY_SERVER, token=cmd_token,
                 priority=0,
-                **galaxy_options,
+                **galaxy_options
             ))
 
         context.CLIARGS['func']()
