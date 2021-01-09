32fd9506d717f6a686ba10cc392a186927352ada
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-01-09 22:34:50 +0100

    fixup! Use raw-strings in `test_galaxy.py`

--- test/units/cli/test_galaxy.py
+++ test/units/cli/test_galaxy.py
@@ -636,8 +636,8 @@ def test_invalid_collection_name_install(name, expected, tmp_path_factory):
     # FIXME: we should add the collection name in the error message
     # Used to be: expected = "Invalid collection name '%s', name must be in the format <namespace>.<collection>" % expected
     expected = "Neither the collection requirement entry key 'name', nor 'source' point to a concrete resolvable collection artifact. "
-    expected += "Also 'name' is not an FQCN\. A valid collection name must be in the format <namespace>\.<collection>\. "
-    expected += "Please make sure that the namespace and the collection name  contain characters from \[a\-zA\-Z0\-9_\] only\."
+    expected += r"Also 'name' is not an FQCN\. A valid collection name must be in the format <namespace>\.<collection>\. "
+    expected += r"Please make sure that the namespace and the collection name  contain characters from \[a\-zA\-Z0\-9_\] only\."
 
     gc = GalaxyCLI(args=['ansible-galaxy', 'collection', 'install', name, '-p', os.path.join(install_path, 'install')])
     with pytest.raises(AnsibleError, match=expected):
@@ -1098,8 +1098,8 @@ def test_parse_requirements_without_mandatory_name_key(requirements_cli, require
     # Should we check that either source or name is provided before using the dep resolver?
 
     expected = "Neither the collection requirement entry key 'name', nor 'source' point to a concrete resolvable collection artifact. "
-    expected += "Also 'name' is not an FQCN\. A valid collection name must be in the format <namespace>\.<collection>\. "
-    expected += "Please make sure that the namespace and the collection name  contain characters from \[a\-zA\-Z0\-9_\] only\."
+    expected += r"Also 'name' is not an FQCN\. A valid collection name must be in the format <namespace>\.<collection>\. "
+    expected += r"Please make sure that the namespace and the collection name  contain characters from \[a\-zA\-Z0\-9_\] only\."
 
     with pytest.raises(AnsibleError, match=expected):
         requirements_cli._parse_requirements_file(requirements_file)
