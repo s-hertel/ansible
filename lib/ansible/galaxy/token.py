10d47008e1ab7c984ba8d25f21a6a52d73610689
s-hertel <19572925+s-hertel@users.noreply.github.com>
2022-10-14 15:26:08 -0400

    Approach 1 (naive)
    add expiration property to KeycloakToken
    
    warn when token expires
    
    handle expired token when waiting for imports to complete after publishing to AH
    
    changelog
    
    (cherry picked from commit 6d915432f1a7aec2778336661bcae7a0ae30dc1b)

--- lib/ansible/galaxy/token.py
+++ lib/ansible/galaxy/token.py
@@ -23,6 +23,8 @@ from __future__ import annotations
 import base64
 import os
 import json
+
+from datetime import datetime, timedelta
 from stat import S_IRUSR, S_IWUSR
 
 from ansible import constants as C
@@ -33,6 +35,9 @@ from ansible.module_utils.urls import open_url
 from ansible.utils.display import Display
 
 display = Display()
+
+
+ISO_8601_FORMAT_STR = "%Y-%m-%dT%H:%M:%S.%f%z"
 
 
 class NoTokenSentinel(object):
@@ -53,10 +58,26 @@ class KeycloakToken(object):
         self.access_token = access_token
         self.auth_url = auth_url
         self._token = None
+        self._issued_at = None
+        self._expires_in = None
         self.validate_certs = validate_certs
         self.client_id = client_id
         if self.client_id is None:
             self.client_id = 'cloud-services'
+
+    @property
+    def expired(self):
+        if None in (self._issued_at, self._expires_in):
+            return False
+        try:
+            issue_time = datetime.strptime(self._issued_at, ISO_8601_FORMAT_STR)
+        except ValueError:
+            display.warning(f"KeycloakToken issued at unknown time format {self._issued_at}")
+            return False
+
+        # expire a few seconds before to handle proactively
+        almost_expiration = issue_time + timedelta(seconds=self._expires_in - 3)
+        return datetime.now(tz=issue_time.tzinfo) > almost_expiration
 
     def _form_payload(self):
         return 'grant_type=refresh_token&client_id=%s&refresh_token=%s' % (self.client_id,
@@ -86,8 +107,10 @@ class KeycloakToken(object):
 
         data = json.loads(to_text(resp.read(), errors='surrogate_or_strict'))
 
-        # - extract 'access_token'
+        # - extract token details
         self._token = data.get('access_token')
+        self._issued_at = data.get('issued_at')
+        self._expires_in = int(data.get('expires_in'))
 
         return self._token
 
